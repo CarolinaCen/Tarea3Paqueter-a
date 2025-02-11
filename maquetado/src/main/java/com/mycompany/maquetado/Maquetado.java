7d0760f91323cd9ab6a7b50264992ede9111f867
CarolinaCen <al066546@uacam.mx>
2025-02-11 11:57:23 -0600

    Add files via upload

---
+++ maquetado/src/main/java/com/mycompany/maquetado/Maquetado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maquetado;
+
+/**
+ *
+ * @author carot
+ */
+public class Maquetado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
